5359abd484acd408951974ffbc6e3288ff85e2c2
Mathew Pang <pangrmx@gmail.com>
2015-11-11 08:13:16 +0000

    Port wire in arduino for use with beaglebone

---
+++ claptrap/src/imu/src/Wire.cpp
@@ -0,0 +1,87 @@
+#include "Wire.h"
+
+void Wire::helloWorld() {
+   //printf("Hello World!");
+    std::cout << "Hello World\n";
+}
+
+/* begin
+ *   Starts up i2c settings
+ */
+void Wire::begin(){
+
+}
+
+int Wire::requestFrom(int address, int quantity){
+    if((i2cFile = open(FILE_PATH, O_RDWR)) < 0){
+        // ERROR HANDLING
+        //   perror(“Failed to open the i2c bus”);
+    }
+    if (ioctl(i2cFile, I2C_SLAVE, address) < 0) {
+      //perror(“i2cSetAddress”);
+      //exit(1);
+    }
+}
+
+/* beginTransmission
+ *  Takes in deviceAddress, opens file descriptor
+ */
+void Wire::beginTransmission(int address){
+    if((i2cFile = open(FILE_PATH, O_RDWR)) < 0){
+    // ERROR HANDLING
+     //   perror(“Failed to open the i2c bus”);
+    }
+    if (ioctl(i2cFile, I2C_SLAVE, address) < 0) {
+      //perror(“i2cSetAddress”);
+      exit(1);
+    }
+}
+
+/* endTransmission
+ *   close i2cFile
+ */
+int Wire::endTransmission(){
+    close(i2cFile);
+}
+
+/* write
+ *
+ */
+int Wire::write(uint8_t byte){
+    if(::write(i2cFile, &byte , 1) != 1) {
+        std::cout << "write error\n";
+    }
+}
+
+int Wire::available(){
+    struct stat st;
+    fstat(i2cFile, &st);
+    return st.st_size;
+}
+
+uint8_t Wire::read(){
+    uint8_t byte = 0x0;
+    if(::read(i2cFile, &byte, 1) != 1){
+       std::cout << "read error\n";
+    }
+    return byte;
+}
+
+void Wire::onReceive(void(*handler) (int)){
+
+} //Unused
+
+void Wire::onRequest(void(*handler) (void)){
+
+} //Unused
+
+
+
+int main(int argc, char **argv) {
+    Wire my_wire = Wire();
+    my_wire.helloWorld();
+
+}
+
+
+
---
+++ claptrap/src/imu/src/Wire.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdint.h> //uint8_t
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#define FILE_PATH "dev/i2c-2"
+
+class Wire {
+    public:
+        void     helloWorld(); //Practice
+        void     begin();
+        int      requestFrom(int address, int quantity);
+        void     beginTransmission(int address);
+        int      endTransmission();
+        int      write(uint8_t byte);
+        int      available();
+        uint8_t  read();
+        void     onReceive(void(*handler) (int)); //Unused
+        void     onRequest(void(*handler) (void)); //Unused
+
+    private:
+        int i2cFile;
+};
