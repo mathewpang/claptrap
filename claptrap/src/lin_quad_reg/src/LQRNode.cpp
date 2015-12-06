e552363b0701978e3d8445ba19f1b310ff1d2c40
Natsumi Otaguro <natsumi.otaguro@gmail.com>
2015-12-06 10:43:50 +0000

    Add publisher to LQR Node

--- claptrap/src/lin_quad_reg/src/LQRNode.cpp
+++ claptrap/src/lin_quad_reg/src/LQRNode.cpp
@@ -1,8 +1,9 @@
 #include "LQRNode.h"
 
 
-LQRNode::LQRNode(IMUObserver* observer){
+LQRNode::LQRNode(IMUObserver* observer, ros::Publisher* pub){
 	this->observer = observer;
+	this->pub = pub;
 }
 
 
@@ -11,7 +12,9 @@ void LQRNode::computeLQR(){
 	struct orientation* orient = observer->getOrientation();
 	//That might be always the same but value is different
 	//std::cout << "or_data roll::" << orient->roll << "orient roll d ::" << orient->roll_d << "\n";
-    
+    geometry_msgs::Twist msg;
+    msg.linear.x = orient->roll*-1*100;
+    pub->publish(msg);
 }
 
 int main(int argc, char **argv) {
@@ -23,10 +26,13 @@ int main(int argc, char **argv) {
     
 
     IMUObserver* observer = new IMUObserver();
-    LQRNode *lqr_node = new LQRNode(observer);
-
     ros::Subscriber sub = n.subscribe("accel_gyro", 1000, &IMUObserver::imuCallback, observer);   
-    
+    ros::Publisher pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
+
+    LQRNode *lqr_node = new LQRNode(observer, &pub);
+
+   
+
     while (ros::ok()){
     	lqr_node->computeLQR();
     	ros::spinOnce();
--- claptrap/src/lin_quad_reg/src/LQRNode.h
+++ claptrap/src/lin_quad_reg/src/LQRNode.h
@@ -3,16 +3,17 @@
 
 #include <ros/ros.h>
 #include "imu/AccelGyro.h"
+#include "geometry_msgs/Twist.h"
 #include "IMUObserver.h"
 
 class LQRNode{
 	
 	public:
-		LQRNode(IMUObserver* observer);
+		LQRNode(IMUObserver* observer, ros::Publisher* pub);
 		void computeLQR();
 	private:
 		IMUObserver* observer; 
-
+		ros::Publisher* pub;
 	
 };
 
